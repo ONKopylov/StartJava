4d403211283e1030be9380bff30c9f05289eb6fe
ONKopylov <kopylov06@inbox.ru>
2022-04-20 00:13:53 +0700

    some commit

--- Lesson2/Jaeger.java
+++ Lesson2/Jaeger.java
@@ -1,6 +1,6 @@
 public class Jaeger {
     
-    String modelName = "Bracer Phoenix";
+    private String modelName = "Bracer Phoenix";
     String mark = "Mark-5";
     String origin = "USA";
     float height = 70.7f;
@@ -9,20 +9,32 @@ public class Jaeger {
     int strength = 8;
     int armor = 9;
 
-    boolean drift() {
+    public String getModelName() {
+        return modelName;
+    }
+
+    void setModelName(String modelName) {
+        if (modelName == "") {
+            System.out.println("Empty string!!!");
+        } else {
+            this.modelName = modelName;
+        }
+    }
+
+    public boolean drift() {
         System.out.println("You are in drift!");
         return true;
     }
 
-    void move() {
+    public void move() {
         System.out.println("Вы прошли 5 шагов!");
     }
 
-    String scanKaiju() {
+    public String scanKaiju() {
         return "nothing";
     }
 
-    void useVortexCannon() {
+    public void useVortexCannon() {
 
     }
 }
--- Lesson2/JaegerTest.java
+++ Lesson2/JaegerTest.java
@@ -2,10 +2,12 @@ public class JaegerTest {
     public static void main(String[] args) {
         //Jaeger jaegerOne;
         Jaeger jaegerOne = new Jaeger();
-        System.out.println("modelName = " + jaegerOne.modelName);
-        System.out.println("speed = mark" + jaegerOne.mark);
+        System.out.println("modelName = " + jaegerOne.getModelName());
+        jaegerOne.setModelName("Java");
+        System.out.println("modelName = " + jaegerOne.getModelName());
+        /*System.out.println("mark = " + jaegerOne.mark);
         System.out.println("speed = " + jaegerOne.speed);
-        System.out.println(jaegerOne.drift());
+        System.out.println(jaegerOne.drift());*/
     }
         
 }
